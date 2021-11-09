5076b3dce155517c8981b30b68c5b47d9fcb8046
danussh <pdanussh1997@gmail.com>
2021-11-09 15:31:02 +0530

    format files

--- src/pages/experience/experience.component.jsx
+++ src/pages/experience/experience.component.jsx
@@ -14,31 +14,49 @@ const Experience = () => {
         <Container>
           <Tilt options={{ max: 25 }}>
             <Card>
-              <Card.Header as="h5" className="d-flex justify-content-center flex-wrap"></Card.Header>
-              <Card.Title className="text-center" style={{color: "#"}}><h1><b>COGNIZANT</b></h1></Card.Title>
- 
+              <Card.Header
+                as="h5"
+                className="d-flex justify-content-center flex-wrap"
+              ></Card.Header>
+              <Card.Title className="text-center" style={{ color: "#" }}>
+                <h1>
+                  <b>COGNIZANT</b>
+                </h1>
+              </Card.Title>
+
               {/* <Card.Body className="d-flex justify-content-center flex-column"> */}
-                <div>
-                  <Card.Title className="text-center"><b>PROGRAMMER ANALYST TRAINEE</b></Card.Title>
-                </div>
-                <div>
-                  <Card.Text className="text-center style">
-                    <strong>Technology:</strong> Mainframe
-                    <br />
-                    <strong>Duration:</strong> NOV 2019 - May 2020
-                    <br/>
-                    <strong> Description </strong>
-                    <ul className="text-left">
-                      <li><strong>Develop </strong> & Code high-performance programs for Clients 
-</li>
-                      <li><strong>Analyze</strong>  user needs and design software solutions
-                      </li>
-                      <li><strong>Provided</strong> application maintenance while working as `Production Support`. 
-                      </li>
-                      <li><strong>Perform</strong> Work with other teams for debugging.</li>
-                    </ul>
-                  </Card.Text>
-                </div>
+              <div>
+                <Card.Title className="text-center">
+                  <b>PROGRAMMER ANALYST TRAINEE</b>
+                </Card.Title>
+              </div>
+              <div>
+                <Card.Text className="text-center style">
+                  <strong>Technology:</strong> Mainframe
+                  <br />
+                  <strong>Duration:</strong> NOV 2019 - May 2020
+                  <br />
+                  <strong> Description </strong>
+                  <ul className="text-left">
+                    <li>
+                      <strong>Develop </strong> & Code high-performance programs
+                      for Clients
+                    </li>
+                    <li>
+                      <strong>Analyze</strong> user needs and design software
+                      solutions
+                    </li>
+                    <li>
+                      <strong>Provided</strong> application maintenance while
+                      working as `Production Support`.
+                    </li>
+                    <li>
+                      <strong>Perform</strong> Work with other teams for
+                      debugging.
+                    </li>
+                  </ul>
+                </Card.Text>
+              </div>
               {/* </Card.Body> */}
             </Card>
           </Tilt>
